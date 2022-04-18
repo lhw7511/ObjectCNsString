8616403ffb55e79beb18dde03eea433474cb697a
hyunwooklee <lhw751@naver.com>
2022-04-18 15:48:09 +0900

    NSString 자체 메소드

--- NSString/main.m
+++ NSString/main.m
@@ -15,8 +15,29 @@ int main(int argc, const char * argv[]) {
         //NSString *str = [[NSString alloc]initWithString:@"This is NSString"];
         
         //java처럼 string의경우만 바로 초기화 가능
-        NSString * str = @"This is NSString";
+        NSString *str = @"This is NSString";
         NSLog(@"str : %@",str);
+        
+        //immutable class
+        //NSString은하나의 인스턴스에 값을 다이렉트로 재할당이 불가하다.
+        NSString *result;
+        //6index부터 끝까지
+        result = [str substringFromIndex:6];
+        NSLog(@"result : %@",result);
+        //0부터 2index까지
+        result = [str substringToIndex:3];
+        NSLog(@"result : %@",result);
+        
+        
+        result = [[str substringToIndex:11]substringFromIndex:8];
+        NSLog(@"result : %@",result);
+        
+        //index 8부터 3길이까지
+        result = [str substringWithRange:NSMakeRange(8, 3)];
+        NSLog(@"result : %@",result);
+        
+        result = [[str substringWithRange:NSMakeRange(8, 3)]lowercaseString];
+        NSLog(@"result : %@",result);
     }
     return 0;
 }
