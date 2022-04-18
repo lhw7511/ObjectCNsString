6a14b3bc49999b5dced316a42132759635a0c540
hyunwooklee <lhw751@naver.com>
2022-04-18 15:16:12 +0900

    Initial Commit

---
+++ NSString/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSString
+//
+//  Created by beintech on 2022/04/18.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
